d4b1712193cd9add969f483272bef6c90eec1721
GitHub Action <action@github.com>
2022-03-09 01:44:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/yeternallove/gitmojiplusplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.yeternallove.gitmojiplusplugin.services
+
+import com.github.yeternallove.gitmojiplusplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/yeternallove/gitmojiplusplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yeternallove/gitmojiplusplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.yeternallove.gitmojiplusplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yeternallove.gitmojiplusplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
